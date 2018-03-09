a14b580d59d079570a76d61f889ea75fffeea3cc
ManHidden <32382445+ManHidden@users.noreply.github.com>
2018-03-09 14:22:16 +0100

    Update player.cpp

--- player.cpp
+++ player.cpp
@@ -1,39 +1,49 @@
-using namespace std;
-
+//
+// Created by patryk on 09.03.18.
+//
 class Player{
 private:
     double PlayerHealth;
     double PlayerHealthMax;
     double PlayerMana;
     double PlayerManaMax;
-
 public:
-    Player(double PlayerHealth, double PlayerHealthMax, double PlayerMana, double PlayerManaMax);
+    Player(double PH, double PHM, double PM, double PMM);
+
+    double getPlayerHealth() const;
+    double getPlayerHealthMax() const;
+    double getPlayerMana() const;
+    double getPlayerManaMax() const;
+
     void setHealth(double current, double max);
     void setMana(double current, double max);
-    double getPlayerHealth() const{
-        return PlayerHealth;
-    };
-    double getPlayerHealthMax() const{
-        return PlayerHealthMax;
-    }
-    double getPlayerMana() const{
-        return PlayerMana;
-    }
-    double getPlayerManaMax() const{
-        return PlayerManaMax;
-    }
+
 };
 
 Player::Player(double PlayerH, double PlayerHM, double PlayerM, double PlayerMM):
-    PlayerHealth(PlayerH),
-    PlayerHealthMax(PlayerHM),
-    PlayerMana(PlayerM),
-    PlayerManaMax(PlayerMM)
+        PlayerHealth(PlayerH),
+        PlayerHealthMax(PlayerHM),
+        PlayerMana(PlayerM),
+        PlayerManaMax(PlayerMM)
 {
 
 }
 
+double Player::getPlayerHealth() const{
+    return PlayerHealth;
+};
+
+double Player::getPlayerHealthMax() const{
+    return PlayerHealthMax;
+}
+
+double Player::getPlayerMana() const{
+    return PlayerMana;
+}
+
+double Player::getPlayerManaMax() const{
+    return PlayerManaMax;
+}
 
 void Player::setHealth(double current, double max){
     PlayerHealth = current;
@@ -44,4 +54,3 @@ void Player::setMana(double current, double max){
     PlayerMana = current;
     PlayerManaMax = max;
 }
-
